2b675672a2d37c07dae2f78e707d3f3693220a71
santisadventures <santisadventures@users.noreply.github.com>
2026-09-02 16:13:22 -0300

    Connect carrier form

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.ECOFLETE_CONFIG = {
   API_URL: "",
-  OFFER_FREIGHT_FORM_URL: "",
+  OFFER_FREIGHT_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSeBVnsnibrUjRdGVZ05hrIHqQiN_zE-90LpZosP1GM--LBXRQ/viewform?usp=dialog",
   REQUEST_FREIGHT_FORM_URL: "",
   INTEREST_FORM_URL: "",
   DEMO_MODE: true,

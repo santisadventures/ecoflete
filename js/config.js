3398da2dbededab7379f50c13a1c1f49a2cf4b65
santisadventures <santisadventures@users.noreply.github.com>
2026-09-07 11:04:08 -0300

    Actualiza links a formularios propios

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.ECOFLETE_CONFIG = {
   API_URL: "data/publicaciones.json",
-  OFFER_FREIGHT_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSeBVnsnibrUjRdGVZ05hrIHqQiN_zE-90LpZosP1GM--LBXRQ/viewform?usp=dialog",
-  REQUEST_FREIGHT_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSf55PbU-XYkAot6ymsCIO44ACuz7DM82MAVRZl9KM3NsDWneg/viewform",
+  OFFER_FREIGHT_FORM_URL: "ofrecer-flete/",
+  REQUEST_FREIGHT_FORM_URL: "buscar-flete/",
   INTEREST_FORM_URL: "",
   DEMO_MODE: false,
   ANALYTICS_ENABLED: false
